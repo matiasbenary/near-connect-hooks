db99cf099138d7c44ed6c67ff5d6bfa6770c1bcc
Matias Benary <matiasbenary@gmail.com>
2025-11-13 16:09:29 -0300

    fix: check githubaction

--- example/next.config.js
+++ example/next.config.js
@@ -1,4 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production'
+const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,6 +10,17 @@ const nextConfig = {
   output: "export",
   distDir: 'build',
   reactStrictMode: true,
+  transpilePackages: ['near-connect-hooks'],
+  outputFileTracingRoot: path.join(__dirname, '../'),
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig;
